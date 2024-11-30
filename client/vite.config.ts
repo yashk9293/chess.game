5f33398a4d788d20a462f42c4afa29ba544028ae
Yash Kumar <yashk9293@gmail.com>
2024-11-30 17:38:05 +0530

    initial

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://chess-game-ten-gray.vercel.app/",
         changeOrigin: true,
       },
     },
